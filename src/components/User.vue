6e878d30c442bb5dbf08f18ba52fc0d00845d8e8
Natalia Pinzon <nanuta522@gmail.com>
2022-12-22 08:16:15 -0500

    add at to users

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,7 +1,7 @@
 <template>
     <span class="user">
         <span class="name">{{ user.name }}</span>
-        <span class="handle">{{ user.handle }}</span>
+        <span class="handle">@{{ user.handle }}</span>
     </span>
 </template>
 
